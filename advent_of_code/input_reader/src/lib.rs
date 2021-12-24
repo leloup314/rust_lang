588f80241edcb1cea130f6af0c6aa8dc2f8a6d0f
leloup314 <wolf@physik.uni-bonn.de>
2021-12-24 20:06:36 +0100

    ADD: reading raw input and returning vec of lines

--- advent_of_code/input_reader/src/lib.rs
+++ advent_of_code/input_reader/src/lib.rs
@@ -14,3 +14,16 @@ pub fn load_ints_from_file(file_path: &str) -> Vec<i64> {
                                .collect();
     ints
 }
+
+
+pub fn load_lines_from_file(file_path: &str) -> Vec<String> {
+    // Open file
+    let f = File::open(file_path).expect("File not found!");
+    // Prepare buffer
+    let reader = BufReader::new(f);
+    // Loop over buffer and fill vector
+    let strs: Vec<String> = reader.lines()
+                                  .map(|line| line.unwrap())
+                                  .collect();
+    strs
+}
