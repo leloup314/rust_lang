1e5a92eaeae0b6927d60e6f21d381739ea70d05d
leloup314 <wolf@physik.uni-bonn.de>
2024-01-08 00:27:27 +0100

    ENH: day 2, part 2

--- advent_of_code/2023/day_2/snow_island/src/main.rs
+++ advent_of_code/2023/day_2/snow_island/src/main.rs
@@ -59,6 +59,69 @@ fn part1(input: &String) -> usize {
 }
 
 
+fn part2(input: &String) -> usize {
+    
+    let mut power_total: usize = 0;
+    
+    #[derive(Debug)]
+    struct ColorMaxVals {
+        red: usize,
+        green: usize,
+        blue: usize
+    }
+
+    impl ColorMaxVals {
+        fn power(&self) -> usize {
+            self.red*self.green*self.blue
+        }
+        
+        fn reset(&mut self) -> () {
+            self.red = 0;
+            self.green = 0;
+            self.blue = 0;
+        }
+        fn update_max(&mut self, color: &str, value: usize) -> () {
+            match color {
+                "red" => { self.red = if value > self.red {value} else {self.red};},
+                "green" =>  { self.green = if value > self.green {value} else {self.green};},
+                "blue" =>  { self.blue = if value > self.blue {value} else {self.blue};},
+                _ => ()
+            }
+        }
+    }
+
+    let mut color_max = ColorMaxVals { red: 0, green: 0, blue: 0 };            
+ 
+    // Loop over each game
+    for line in input.lines(){
+
+        if !line.is_empty() {
+        
+            let game_data = line.split(":");
+  
+            let game_sets = game_data.last().unwrap();
+            
+            color_max.reset();
+            
+            for gs in game_sets.split_terminator(";") {
+
+                for colors in gs.split_terminator(",") {
+                    
+                    let mut clr = colors.split_whitespace();
+                    let value: usize = clr.next().unwrap().parse().unwrap();
+                    let clr = clr.next().unwrap();
+
+                    color_max.update_max(&clr, value);
+                
+                }
+            }
+            power_total += color_max.power();
+        }
+    }
+    power_total
+}
+
+
 
 
 fn main() {
@@ -69,6 +132,7 @@ fn main() {
     let input = String::from_utf8(std::fs::read(input_file).unwrap()).unwrap();    
     
     let result_part1 = part1(&input);
-    
     println!("{}", result_part1);
+    let result_part2 = part2(&input);
+    println!("{}", result_part2);
 }
